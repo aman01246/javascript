a8671e561e8bc5ba06f4de7aa7e7a2870d1a57c3
aman01246 <amankumar01246@gmail.com>
2025-07-06 20:27:29 +0530

    change

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+//number 
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+
+
+let a=null
+console.log(typeof(a));
+a=13
+b='aman'
+c=true
+d=undefined
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
--- variable.js
+++ variable.js
@@ -2,3 +2,9 @@ const accountId= 12345
 let name ="aman kumar";
 var title = "kumar";
 console.log(name);
+name = "Rohit Gupta";
+title ="prakash "
+var amount="50000"
+name ="prakash"
+console.table([name,title,amount])
+
